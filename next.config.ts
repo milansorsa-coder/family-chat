108a04092b877f31788651f24e84a587c7443b0a
milansorsa-coder <milan.sorsa@gmail.com>
2026-02-14 17:06:06 +0000

    to use on phone

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export', // This creates the 'out' folder needed for Android
+  images: {
+    unoptimized: true, // Required because Android can't run the Next.js image server
+  },
 };
 
 export default nextConfig;
